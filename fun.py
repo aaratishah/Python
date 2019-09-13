c0d9faf4092d6d563637e9cd215ac8822deec839
aaratishah <aaratishah2057@gmail.com>
2019-09-13 10:40:59 +0545

    source code

---
+++ fun.py
@@ -0,0 +1,13 @@
+import random 
+
+
+def func():
+    dice = [1, 2, 3, 4, 5, 6]
+    for n in dice:
+        print("you got " + str(random.choice(dice)))
+        break
+       
+
+m = input ("enter anything to roll the dice ")
+func()    
+
