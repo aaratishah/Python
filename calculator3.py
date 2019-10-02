6c3f8d103e04ebef61f3805b8833a4ae71dabd25
aaratishah <aaratishah2057@gmail.com>
2019-10-02 12:42:34 +0545

    calculator

--- calculator3.py
+++ calculator3.py
@@ -1,3 +1,4 @@
+from math import *
 import sys
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
@@ -12,78 +13,118 @@ SubtractButton = QPushButton("-", widget)
 MultiplyButton = QPushButton("*", widget)
 DivideButton = QPushButton("/", widget)
 PercentButton = QPushButton("%", widget)
+TangentButton = QPushButton("tan", widget)
+SineButton = QPushButton("sin", widget)
+CosineButton = QPushButton("cos", widget)
+FactButton = QPushButton("!", widget)
 
 txtArea1 = QLineEdit("", widget)
 txtArea2 = QLineEdit("", widget)
 
 def init():
-    widget.resize(250, 300)
-    widget.move(250, 300)
+    widget.resize(250, 350)
+    widget.move(250, 350)
     widget.setWindowTitle('Calculator')
     widget.show()
  
     txtArea1.move(20, 30)
     txtArea2.move(20, 80)
-    
- 
+     
     label.setText("")
     label.move(20, 110)
-    
- 
+    label.show()
+     
     AddButton.setToolTip("Addition")
     AddButton.move(20, 160)
     AddButton.clicked.connect(addition)
-    
- 
+     
     SubtractButton.setToolTip("Subtraction")
     SubtractButton.move(110, 160)
     SubtractButton.clicked.connect(subtraction)
-    
- 
+     
     DivideButton.setToolTip("Division")
     DivideButton.move(20, 200)
     DivideButton.clicked.connect(division)
-    
- 
+     
     MultiplyButton.setToolTip("Multiplication")
     MultiplyButton.move(110, 200)
     MultiplyButton.clicked.connect(multiplication)
    
-
     PercentButton.setToolTip("Percentage")
-    PercentButton.move(70,235)
+    PercentButton.move(20, 240)
     PercentButton.clicked.connect(percent)
 
- 
+    TangentButton.setToolTip("Tangent")
+    TangentButton.move(110, 240)
+    TangentButton.clicked.connect(tangent)
+
+    SineButton.setToolTip("Sine")
+    SineButton.move(20, 280)
+    SineButton.clicked.connect(sine)
+
+    CosineButton.setToolTip("Cosine")
+    CosineButton.move(110, 280)
+    CosineButton.clicked.connect(cosine)
+
+    FactButton.setToolTip("Factorial")
+    FactButton.move(20, 320)
+    FactButton.clicked.connect(factorials)
+
  
 def addition():
     num1 = int(txtArea1.text())
     num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
     label.setText(str(num1) + "+" + str(num2) + "=" + str(num1 + num2))
  
  
 def subtraction():
     num1 = int(txtArea1.text())
     num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
     label.setText(str(num1) + "-" + str(num2) + "=" + str(num1 - num2))
  
  
 def multiplication():
     num1 = int(txtArea1.text())
     num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
     label.setText(str(num1) + "*" + str(num2) + "=" + str(num1 * num2))
  
  
 def division():
     num1 = int(txtArea1.text())
     num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
     label.setText(str(num1) + "/" + str(num2) + "=" + str(num1 / num2))
 
 def percent():
     num1 = int(txtArea1.text())
+    label.setFixedWidth(200)
     label.setText(str(num1/100))
 
- 
+def tangent():
+    num1 = int(txtArea1.text())
+    #num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
+    label.setText("tan" + str(num1) + "=" + str(tan(num1)))
+
+def sine():
+    num1 = int(txtArea1.text())
+    #num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
+    label.setText("sin" + str(num1) + "=" + str(sin(num1)))
+
+def cosine():
+    num1 = int(txtArea1.text())
+    #num2 = int(txtArea2.text())
+    label.setFixedWidth(200)
+    label.setText("cos" + str(num1) + "=" + str(cos(num1)))
+
+def factorials():
+    num1 = int(txtArea1.text())
+    label.setFixedWidth(200)
+    label.setText(str(num1) + " !" + " = " + str(factorial(num1))) 
  
 if __name__ == "__main__":
     init()
